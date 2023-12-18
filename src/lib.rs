85f41431ba0f3ce4deee154e9403c37dd0bc85c2
Paul Fornage <36117326+paulwrath1223@users.noreply.github.com>
2023-12-18 09:32:32 -0800

    clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,2 @@
 mod gpib_controller;
 mod errors;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
